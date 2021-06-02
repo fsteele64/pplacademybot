f7d5c7db6a20754385d97d4fea1d5fd3885ac49a
fsteele64 <80225359+fsteele64@users.noreply.github.com>
2021-06-02 00:43:13 -0400

    Add files via upload

---
+++ pplacademybotv1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+
+import requests
+import time
+
+while True: 
+    try:
+
+        messages = ['Daily Reminder: Update CPP Student Chats \n \n 1. What did you complete with your student today? \n 2. Is your student still on schedule? \n 3. How many hours are left in their package? \n 4. How many estimated hours until course completion?']
+
+        time.sleep(10)
+
+        for message in messages:
+    
+            base_url = 'https://api.telegram.org/bot1724131473:AAGd1fzBu4owagZxIRl-sWNzMRxXemzYv7A/sendMessage?chat_id=-546983246&text={}'.format(message)
+            print(base_url)
+            requests.get(base_url)
+    
+    except Exception as error:
+        print(error)
+       
+    
+
+
+# In[ ]:
+
+
+
+
